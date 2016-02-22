140bbcbe0174159d3c9085f7d1c5af778672c345
cexafaulkner <cexafaulkner@i-sight.com>
2016-02-21 22:32:43 -0500

    minor

--- client/components/login-box/LoginBoxVanilla.jsx
+++ client/components/login-box/LoginBoxVanilla.jsx
@@ -1,5 +1,8 @@
 var ReactGridLayout = require('react-grid-layout');
 
+import {styles} from './login-box-styles.jsx';
+console.log(styles);
+
 var loginGrid = React.createClass({
 	render: function() {
 		return (
@@ -9,21 +12,22 @@ var loginGrid = React.createClass({
 });
 
 
-
 export default React.createClass({
 	render: function() {
 		return (
-			<form method="POST">
-				<span>Username:</span>
-				<input type="text" name="username" />
-				<div>
-					<span>Password:</span>
-					<input type="text" name="password" />
-				</div>
-				<div>
-					<input type="submit" value="Submit" />
-				</div>
-			</form>
+			<div style={styles.formContainer}>
+				<form method="POST" style={styles.form}>
+					<div>Username:</div>
+					<input type="text" name="username" />
+					<div>
+						<div>Password:</div>
+						<input type="text" name="password" />
+					</div>
+					<div>
+						<input type="submit" value="Submit" style={styles.submit} />
+					</div>
+				</form>
+			</div>
 		);
 	}
 });
---
+++ client/components/login-box/login-box-styles.jsx
@@ -0,0 +1,19 @@
+export const styles = {
+	classes: {
+		loginTitle: {
+			'font-style': ''
+		}
+	},
+	formContainer: {
+		'textAlign': 'center',
+		width: '800px',
+		margin: '0 auto'
+	},
+	form: {
+		'textAlign': 'left',
+		'display': 'inline-block'
+	},
+	submit: {
+		'marginTop': '10px'
+	}
+};
