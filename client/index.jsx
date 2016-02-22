350d2aece7b72dbac15b0858f6c0c8b81778d995
cexafaulkner <cexafaulkner@i-sight.com>
2016-02-21 22:44:57 -0500

    more dir org; installed material-ui

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,11 @@
 // components
 import CommentBox from './components/forms/comment-box/CommentBox.jsx';
-import { LikeButton } from './components/like-button/LikeButton.jsx';
-import { AffinityButton } from './components/like-button/AffinityButton.jsx';
-import NavbarTest from './components/navbar-test/NavbarTest.jsx';
-import ImageUploader from './components/image-uploader/ImageUploader.jsx';
+import { LikeButton } from './components/forms/like-button/LikeButton.jsx';
+import { AffinityButton } from './components/forms/like-button/AffinityButton.jsx';
+import NavbarTest from './components/nav/navbar-test/NavbarTest.jsx';
 import { Router, Route, hashHistory, Link } from 'react-router';
 import { FormBox } from './components/sample-class-components/TestClassBasedComponent.jsx';
-import LoginBox from './components/login-box/LoginBoxVanilla.jsx';
+import LoginBox from './components/forms/login-box/LoginBoxVanilla.jsx';
 
 ReactDOM.render(
 	<Router history={hashHistory}>
