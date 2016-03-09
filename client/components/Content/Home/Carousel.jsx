1dab1daedb77680715e0316b94595a4835d0decb
cexafaulkner <cexafaulkner@i-sight.com>
2016-03-09 07:01:18 -0500

    adjusts # img shown based on screen size

--- client/components/Content/Home/Carousel.jsx
+++ client/components/Content/Home/Carousel.jsx
@@ -1,67 +1,8 @@
-// var React = React || require('react');
-// var ReactDOM = ReactDOM || require('react-dom');
-// import { ReactRouter, Link, Router, Route, hashHistory } from 'react-router';
-// var Slider = require('react-slick');
-
-// const style = require('./CarouselStyle.jsx')
-
-// const imgs = [
-//   { filename: '/img/patterns/aboriginal-fungus.jpg' },
-//   { filename: '/img/patterns/berry-flowers.jpg' },
-//   { filename: '/img/patterns/diamond-mine.jpg' },
-//   { filename: '/img/patterns/lsd-butterflies.jpg' },
-//   { filename: '/img/patterns/red-zygote-flowers.jpg' },
-//   { filename: '/img/patterns/rock-eyes.jpg' },
-// ];
-
-// var Carousel = React.createClass({
-//   render: function () {
-//     var settings = {
-//       infinite: true,
-//       slidesToShow: 3,
-//       slidesToScroll: 1,
-//       initialSlide: 1,
-//       draggable: true,
-//       autoplay: true,
-//       speed: 500,
-//       centerMode: true,
-//       useCSS: true,
-//       adaptiveHeight: true,
-//       dots: false,
-//       lazyLoad: true,
-//       prevArrow: CarouselArrowButtonLeft,
-//       nextArrow: CarouselArrowButtonRight,
-//       style: {
-//       	position: 'absolute',
-//       	display: 'inline-block'
-//       }
-//     };
-//     return (
-//       <div className='carousel'>
-//         <Slider {...settings}>
-//           {imgs.map((imgObj) => (
-//             <div style={style.carouselImgContainer}>
-//               <img 
-//                 {...this.props}
-//                 src={imgObj.filename}
-//                 style={style.carouselImg}
-//               />
-//             </div>
-//           ))}
-//         </Slider>
-//       </div>
-//     );
-//   }
-// });
-
-
-// export default Carousel;
-
-
-
-
-
-
+/**************************************************************************************************
+*
+*      Sliding showcase gallery on Home page
+*
+*/
 
 var React = React || require('react');
 var ReactDOM = ReactDOM || require('react-dom');
@@ -112,22 +53,39 @@ export default class Carousel extends React.Component {
       lazyLoad: false,
       arrows: true,
       prevArrow: CarouselArrowButtonLeft,
-      nextArrow: CarouselArrowButtonRight
+      nextArrow: CarouselArrowButtonRight,
+      // adjust number of images shown based on screen size
+      responsive: [
+        { breakpoint: 100000, settings: { slidesToShow: 5 } },
+        { breakpoint: 1300, settings: { slidesToShow: 4 }},
+        { breakpoint: 1000, settings: { slidesToShow: 3 }},
+        { breakpoint: 580, settings: { slidesToShow: 2 }},
+        { breakpoint: 390, settings: { slidesToShow: 1 }}
+      ]
     };
     return (
       <div className='carousel'>
         <Slider {...settings}>
-          <img src='/img/patterns/aboriginal-fungus.jpg' style={{maxHeight: '220px'}} />
-          <img src='/img/patterns/berry-flowers.jpg' style={{maxHeight: '220px'}} />
-          <img src='/img/patterns/diamond-mine.jpg' style={{maxHeight: '220px'}} />
-          <img src='/img/patterns/lsd-butterflies.jpg' style={{maxHeight: '220px'}} />
-          <img src='/img/patterns/red-zygote-flowers.jpg' style={{maxHeight: '220px'}} />
-          <img src='/img/patterns/rock-eyes.jpg' style={{maxHeight: '220px'}} />
+          <CarouselImage src='/img/patterns/aboriginal-fungus.jpg' style={{height: '220px'}} />
+          <CarouselImage src='/img/patterns/berry-flowers.jpg' style={{height: '220px'}} />
+          <CarouselImage src='/img/patterns/diamond-mine.jpg' style={{height: '220px'}} />
+          <CarouselImage src='/img/patterns/lsd-butterflies.jpg' style={{height: '220px'}} />
+          <CarouselImage src='/img/patterns/red-zygote-flowers.jpg' style={{height: '220px'}} />
+          <CarouselImage src='/img/patterns/rock-eyes.jpg' style={{height: '220px'}} />
         </Slider>
       </div>
     );
   }
   componentDidMount() {
     console.log('componentDidMount!')
+  }
+};
+
+class CarouselImage extends React.Component {
+  render() {
+    console.log('Carousel.jsx:: CarouselImage: this.props:', this.props);
+    return (
+      <img src={this.props.src} style={this.props.style} />
+    );
   }
 };
