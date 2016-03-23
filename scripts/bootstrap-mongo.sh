8946b710f11f4ffaa998dfd8c4641f15335e2ab3
andfaulkner <andfaulkner@gmail.com>
2016-03-23 19:50:56 -0400

    temp commented out bootstrap mongo script

--- scripts/bootstrap-mongo.sh
+++ scripts/bootstrap-mongo.sh
@@ -3,16 +3,16 @@
 echo '\n\n\n-----------------------------------------------------\n'
 echo '*** Importing initial MongoDB data ***'
 echo '\nImporting collection "bio":\n'
-mongoimport --db PATTERN_SHOWCASE --collection bio --file ./scripts/mongo-bootstrap-data/bio.json --jsonArray
+# mongoimport --db PATTERN_SHOWCASE --collection bio --file ./scripts/mongo-bootstrap-data/bio.json --jsonArray
 
-echo '\nImporting collection "designs":\n'
-mongoimport --db PATTERN_SHOWCASE --collection designs --file ./scripts/mongo-bootstrap-data/designs.json --jsonArray
+# echo '\nImporting collection "designs":\n'
+# mongoimport --db PATTERN_SHOWCASE --collection designs --file ./scripts/mongo-bootstrap-data/designs.json --jsonArray
 
-echo '\nImporting collection "images":\n'
-mongoimport --db PATTERN_SHOWCASE --collection images --file ./scripts/mongo-bootstrap-data/images.json --jsonArray
+# echo '\nImporting collection "images":\n'
+# mongoimport --db PATTERN_SHOWCASE --collection images --file ./scripts/mongo-bootstrap-data/images.json --jsonArray
 
-echo '\nImporting collection "updates":\n'
-mongoimport --db PATTERN_SHOWCASE --collection updates --file ./scripts/mongo-bootstrap-data/updates.json --jsonArray
+# echo '\nImporting collection "updates":\n'
+# mongoimport --db PATTERN_SHOWCASE --collection updates --file ./scripts/mongo-bootstrap-data/updates.json --jsonArray
 
 # echo '\n-----------------'
 # echo '\nImporting "bio": path attempt 1:\n'
